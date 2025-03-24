46ecf0cff0e8d0d758a95ea298bc2f9365e95f65
Jasn <jasn74939@gmail.com>
2025-03-24 16:35:58 -0400

    update all

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const video = document.getElementById('videoElement');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-
 async function setupCamera() {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-  });
-  video.srcObject = stream;
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+    });
+    video.srcObject = stream;
+  } catch (err) {
+    console.error('Error accessing camera:', err);
+  }
 }
 
 let net;
